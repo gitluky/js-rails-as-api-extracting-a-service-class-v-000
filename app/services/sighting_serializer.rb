d6aa12ed61324c7f578902095fc722f772867453
Jonathan Luk <jluk87@gmail.com>
2019-10-11 21:21:11 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -7,13 +7,13 @@ class SightingSerializer
   def to_serialized_json
     options = {
       include: {
-        bird: { 
+        bird: {
           only: [:name, :species]
         },
-        location: { 
+        location: {
           only: [:latitude, :longitude]
         }
-      }, 
+      },
       except: [:updated_at])
     }
     @sighting.to_json(options)
